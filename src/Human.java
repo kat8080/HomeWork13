50782da687248d232aadbe0fa3b35c4d669a22ba
Katya <K_at@mail.ru>
2022-10-31 14:38:01 +0800

    Домашнее задание 1, задание 2

--- src/Human.java
+++ src/Human.java
@@ -2,8 +2,9 @@ public class Human {
     String name;
     int yearOfBirth;
     String town;
+    String job;
 
     public String toString() {
-        return "Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился в " + yearOfBirth + " году. Будем знакомы!";
+        return "Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился в " + yearOfBirth + " году. Я работаю на должности " + job + ". Будем знакомы!";
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,25 @@ public class Main {
         maksim.name = "Максим";
         maksim.yearOfBirth = 1988;
         maksim.town = "Минск";
+        maksim.job = "бренд-менеджером";
 
         Human anay = new Human();
         anay.name = "Аня";
         anay.yearOfBirth = 1993;
         anay.town = "Москва";
+        anay.job = "методистом образовательных программ";
 
         Human katay = new Human();
         katay.name = "Катя";
         katay.yearOfBirth = 1992;
         katay.town = "Калининград";
+        katay.job = "продакт-менеджером";
 
         Human artem = new Human();
         artem.name = "Артем";
         artem.yearOfBirth = 1995;
         artem.town = "Москва";
+        artem.job = "директором по развитию бизнеса";
 
         System.out.println(maksim);
         System.out.println(anay);
